4b1a4da1bbae6a157695fd03ae731c66d6e63428
maku <190009p@ed.fuk.kindai.ac.jp>
2021-11-22 07:09:13 +0000

    test.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def L4
+        session[:c] ||= 0
+        session[:c] = session[:c].to_i + 1
+        render plain: session.inspect
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-    def main
-        if session[:login_uid]
-            render 'index'
-        else
-            render 'login'
-        end
-    end
-    
     def login
         if params[:uid] == "kindai" and params[:pass] == "sanriko"
             session[:login_uid] = params[:uid]
@@ -14,10 +6,5 @@ class UsersController < ApplicationController
         else
             render 'login_failed'
         end
-    end
-    
-    def logout
-        session.delete(:login_uid)
-        redirect_to root_path
     end
 end
