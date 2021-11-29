845c410600db937768307c9ab9d1743f1739d39b
maku <190009p@ed.fuk.kindai.ac.jp>
2021-11-29 06:31:35 +0000

    test.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+    def main
+        if session[:login_uid]
+            render 'tops/index'
+        else
+            render 'login'
+        end
+    end
+    
     def login
-        if params[:uid] == "kindai" and params[:pass] == "sanriko"
-            session[:login_uid] = params[:uid]
-            redirect_to root_path
+        if params[:age] == "kindai" and params[:gender] == "sanriko"
+            session[:login_uid] = params[:age]
+            redirect_to tops_path
         else
             render 'login_failed'
         end
+    end
+    
+    def logout
+        session.delete(:login_uid)
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     resources :tops
     root 'tops#index'
     
-    resource :users
+    get 'users/main'
+    post 'users/login'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
