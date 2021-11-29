3737fa48f2c606340abd1a8bfebf509b86ef03de
Yamagata Kazuma <1911140065d@ed.fuk.kindai.ac.jp>
2021-11-29 08:24:30 +0000

    mkji

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
     resources :tops
     root 'tops#index'
+    
+    get 'searches/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_085211) do
+ActiveRecord::Schema.define(version: 2021_11_22_073407) do
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "age"
